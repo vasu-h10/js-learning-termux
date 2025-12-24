b4cdac87449413473204e0c0edeba5201d892d66
vasu-h10 <vasu-h10@users.noreply.github.com>
2025-12-24 08:57:26 +0530

    learn: variables with let and const

---
+++ javascript/basics/variables.js
@@ -0,0 +1,14 @@
+// Variables in JavaScript
+
+let name = "Vasu";
+const country = "India";
+
+console.log("Name:", name);
+console.log("Country:", country);
+
+// Try changing name
+name = "Gudiyada";
+console.log("Updated name:", name);
+
+// Uncomment below to see error
+// country = "USA";
